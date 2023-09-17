0ef092824a300b73d7180f28e43b726d268ea125
jios-git <ethancampbell@live.co.uk>
2023-09-17 23:08:46 +0100

    custom topic widgets made (not functional)

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -17,6 +17,7 @@ class MenuPage extends StatefulWidget {
 
 class _MenuPageState extends State<MenuPage> {
   late List<Map<String, String>> topics = [];
+  String selectedTopic = 'Option 1';
 
   @override
   void initState() {
@@ -52,52 +53,133 @@ class _MenuPageState extends State<MenuPage> {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              height: 75,
-              width: 185,
-              child: Card(
-                clipBehavior: Clip.antiAlias,
-                color: const Color.fromARGB(255, 75, 75, 75),
-                child: Row(
-                  children: [
-                    // This Expanded ensures the text takes as much space as available
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Custom",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Make a New Topic"),
+                      content: SingleChildScrollView( // Use SingleChildScrollView to avoid overflow issues
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(style: TextStyle(fontSize: 15),"Create a custom topic following the examples provided in the info button to the right. Clicking \"Load Chat\" will redirect to chat using the custom topic."),
+                            SizedBox(height: 10),
+                            Text("Saved Topics:"),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: DropdownButton<String>(
+                                isExpanded: true,
+                                value: selectedTopic,
+                                onChanged: (String? selection) {
+                                  // Update the state accordingly
+                                  selectedTopic = selection!;
+                                },
+                                items: <String>['Option 1', 'Option 2', 'Option 3']
+                                  .map<DropdownMenuItem<String>>((String value) {
+                                    return DropdownMenuItem<String>(
+                                      value: value,
+                                      child: Text(value),
+                                    );
+                                  })
+                                  .toList(),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Text("Leave the text fields blank if using a saved topic. Create new topic:"),
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: "short title",
+                              ),
+                            ),
+                            TextField(
+                              maxLines: null, // Makes it multiline
+                              keyboardType: TextInputType.multiline,
+                              decoration: InputDecoration(
+                                hintText: "full topic description following example",
+                                hintStyle: TextStyle(
+                                  fontSize: 14.0, // Adjust the size as needed
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: TextButton(
+                                onPressed: () {
+                                  // Handle button press
+                                },
+                                child: Text("Save"),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                    // Container for the decorative Icon with a different background color
-                    Expanded(
-                      child: Container(
-                        alignment: Alignment.centerRight,
-                        decoration: BoxDecoration(
-                          color: Colors.deepOrange[300],
-                          border: Border(
-                            left: BorderSide(
-                              color: Colors.black,
-                              width: 3.0,
+                      actions: [
+                        TextButton(
+                          child: Text("Load Chat"),
+                          onPressed: () {
+                            // Add the functionality for what should happen when "Load Chat" is pressed
+                          },
+                        ),
+                        TextButton(
+                          child: Text("Close"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Container(
+                height: 75,
+                width: 185,
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  color: const Color.fromARGB(255, 75, 75, 75),
+                  child: Row(
+                    children: [
+                      // This Expanded ensures the text takes as much space as available
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Custom",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
                             ),
                           ),
                         ),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 3),
-                          child: Image.asset(
-                            'assets/cog.png',
-                            fit: BoxFit.contain,
+                      ),
+                      // Container for the decorative Icon with a different background color
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.centerRight,
+                          decoration: BoxDecoration(
+                            color: Colors.deepOrange[300],
+                            border: Border(
+                              left: BorderSide(
+                                color: Colors.black,
+                                width: 3.0,
+                              ),
+                            ),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 3),
+                            child: Image.asset(
+                              'assets/cog.png',
+                              fit: BoxFit.contain,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -111,8 +193,24 @@ class _MenuPageState extends State<MenuPage> {
                   context: context,
                   builder: (context) {
                     return AlertDialog(
-                      title: Text('Information'),
-                      content: Text('You can type any language you want, but the pre-set options are what ChatGPT has been trained on the most/fluent in'),
+                      title: Text('How to Make a Topic'),
+                      content: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SelectableText(
+                              'You can type a custom topic for language practice, but make sure it closely follows the format of the following examples:',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 15),  // Add some spacing
+                            SelectableText('You (assistant) should act like a shop clerk. I (the user) will respond as the customer.'),
+                            SizedBox(height: 10),  // Add some spacing
+                            SelectableText('You (assistant) should act like a language teacher meeting me (the user) one-on-one for the first time. The user will respond as the pupil.'),
+                            SizedBox(height: 10),  // Add some spacing
+                            SelectableText('You (assistant) are a Job Interviewer, I (the user) will respond as a Candidate who arrived late.')
+                          ],
+                        ),
+                      ),
                       actions: [
                         TextButton(
                           onPressed: () {
